c7260de84f7dfcaea7ba53e08ac121049e538f21
Moein <mohammadmoein.moradi@gamil.com>
2020-08-08 10:34:10 +0430

    positioning elements of the game. created beginning menu of thee game

---
+++ app.js
@@ -0,0 +1,7 @@
+scoreItemss = document.getElementsByClassName('score');
+matchItems = document.getElementsByClassName('match');
+for (var item of scoreItemss)
+    item.className = 'hide';
+
+for (var item of matchItems)
+    item.className = 'hide';
