cb98cea802716d6112c5ed5b02055756de423795
Moein <mohammadmoein.moradi@gamil.com>
2020-08-08 15:41:24 +0430

    added animation to hands and logic of hiding and showing other elements while the animation is playing

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-scoreItemss = document.getElementsByClassName('score');
-matchItems = document.getElementsByClassName('match');
-for (var item of scoreItemss)
-    item.className = 'hide';
+const scoreSection = document.getElementsByClassName('score')[0];
+const matchSection = document.getElementsByClassName('match')[0];
+const optionsElements = document.querySelectorAll('.options');
 
-for (var item of matchItems)
-    item.className = 'hide';
+var computerScore = 0;
+var playerScore = 0;
+
+hideElement(scoreSection);
+hideElement(matchSection);
+
+document.getElementById("initiate_game").addEventListener('click', beginGame);
+optionButtons = document.getElementsByClassName('options-button');
+for (var i = 0; i < optionButtons.length; i++){
+    optionButtons[i].addEventListener('click', optionClicked);
+}
+
+function optionClicked(){
+
+    document.querySelector('.hands').classList.add('shake');
+    for (var item of optionsElements)
+        hideElement(item);
+
+    let animationTime = 900; // 900 is the duration that it takes to finish the shake animation
+    let buttonClicked = this;
+    setTimeout(function () {
+
+        if (buttonClicked.id === 'rock_button')
+            console.log('ROCK');
+        else if (buttonClicked.id === 'paper_button')
+            console.log('PAPER');
+        else if (buttonClicked.id === 'scissors_button')
+            console.log('SCISSORS');
+
+        document.querySelector('.hands').classList.remove('shake');
+        for (var item of optionsElements)
+            showElement(item);
+    }, animationTime);
+
+
+
+}
+
+function beginGame() {
+    var introSection = document.getElementsByClassName('intro')[0];
+    hideElement(introSection);
+
+    showElement(scoreSection);
+    showElement(matchSection);
+}
+
+function showElement(element){
+    element.classList.remove('hide');
+}
+function hideElement(element){
+    element.classList.add('hide');
+}
